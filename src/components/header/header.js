3b947a4101265832cda3404f4beabdab42912c9e
bz66 <bomber.w.zhang@newegg.com>
2016-04-27 20:36:41 +0800

    add tabs

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 import IconButton from 'material-ui/lib/icon-button';
 import NavigationMenu from 'material-ui/lib/svg-icons/navigation/menu';
-import { AppBar } from 'material-ui';
+import { AppBar, Tabs, Tab } from 'material-ui';
 import Logo from './Logo';
 
+import VilaenThemeLight from '../../styles/Themes/VilaenThemeLight';
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+
+const themeStyle = getMuiTheme(VilaenThemeLight); 
+
+const tabsContainerStyle = {
+    'position': 'absolute',
+    'height': themeStyle.appBar.height,
+    'line-height': themeStyle.appBar.height, 
+    'left': '40%',
+    'overflow': 'hidden',
+    'margin': '0 auto',
+}; 
+
+const tabsStyle ={
+    
+};
 
 class Header extends Component {
 
     render() {
         return (
             <AppBar iconElementLeft={<IconButton onClick={this.props.onMainMenuClick}><NavigationMenu /></IconButton>}>
+                <Logo />
+                <div style={tabsContainerStyle}>
+                    <Tabs>
+                        <Tab label="Item One">
+                        </Tab>
+                        <Tab label="Item Two">
+                        </Tab>
+                    </Tabs>                
+                </div>
             </AppBar>
         )
 
@@ -17,7 +43,7 @@ class Header extends Component {
 }
 
 Header.propTypes = {
-     onMainMenuClick: PropTypes.object,
+    onMainMenuClick: PropTypes.object,
 };
 
 export default Header;
