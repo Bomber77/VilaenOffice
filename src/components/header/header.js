38cc8c6f56c8fd11e7438a36d02064a85b68bb23
bz66 <bomber.w.zhang@newegg.com>
2016-08-23 14:43:04 +0800

    fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component, PropTypes } from 'react';
 import IconButton from 'material-ui/lib/icon-button';
 import NavigationMenu from 'material-ui/lib/svg-icons/navigation/menu';
@@ -52,59 +51,4 @@ Header.propTypes = {
     onMainMenuClick: PropTypes.object,
 };
 
-=======
-import React, { Component, PropTypes } from 'react';
-import IconButton from 'material-ui/lib/icon-button';
-import NavigationMenu from 'material-ui/lib/svg-icons/navigation/menu';
-import { AppBar, Tabs, Tab, Avatar } from 'material-ui';
-import Logo from './Logo';
-import User from './user';
-
-import VilaenThemeLight from '../../styles/Themes/VilaenThemeLight';
-import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
-
-
-const themeStyle = getMuiTheme(VilaenThemeLight);
-const spaceOfTabsRight = 2; 
-
-const tabsContainerStyle = {
-    'position': 'absolute',
-    'dispaly': 'inline-block',
-    'float': 'left',
-    'right': themeStyle.appBar.height * spaceOfTabsRight,
-    'height': themeStyle.appBar.height,
-    'line-height': themeStyle.appBar.height, 
-    'overflow': 'hidden',
-}; 
-
-const tabsStyle = {
-    
-};
-
-class Header extends Component {
-
-    render() {
-        return (
-            <AppBar iconElementLeft={<IconButton onClick={this.props.onMainMenuClick}><NavigationMenu /></IconButton>}>
-                <Logo />
-                <div style={tabsContainerStyle}>
-                    <Tabs>
-                        <Tab label="Booking">
-                        </Tab>
-                        <Tab label="Build Room">
-                        </Tab>
-                    </Tabs> 
-                </div>
-                <User />
-            </AppBar>
-        )
-
-    }
-}
-
-Header.propTypes = {
-    onMainMenuClick: PropTypes.object,
-};
-
->>>>>>> 5e63e0a74f4da842f34a6f5326049916fd6f69b0
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -13,19 +12,3 @@ injectTapEventPlugin();
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
-=======
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/Main';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
-
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
->>>>>>> 5e63e0a74f4da842f34a6f5326049916fd6f69b0
